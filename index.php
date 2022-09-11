9622aaae611069a10f55d748c55172e2e3cf0588
Liset Sintia Umami <lisetzayn17@gmail.com>
2022-09-11 07:56:01 +0700

    menambhakan list

--- index.php
+++ index.php
@@ -6,5 +6,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum commodi sit vel harum aspernatur ullam qui, sint rem natus quod aperiam maiores veritatis voluptatibus atque cumque alias. Ea, magni accusantium!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+</ul>
 </body>
 </html>
